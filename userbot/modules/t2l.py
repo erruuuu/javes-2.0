f6bdd52018f09e006e01376f94991c39e5b238ba
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2020-11-21 19:00:55 +0530

    Update and rename tool2.py to t2l.py

--- userbot/modules/t2l.py
+++ userbot/modules/t2l.py
@@ -533,43 +533,6 @@ Answers: \n""".format(closed_status, question)
         return await rkp.edit(edit_caption)
 
 
-@javes.on(rekcah05(pattern=f"goss(.*)", allow_sudo=True))
-@javes05(outgoing=True, pattern="^!goss(.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    if Config.SCREEN_SHOT_LAYER_ACCESS_KEY is None:
-        await event.edit("Need to get an API key from https://screenshotlayer.com/product \nModule stopping!")
-        return
-    await event.edit("Processing ...")
-    sample_url = "https://api.screenshotlayer.com/api/capture?access_key={}&url={}&fullpage={}&viewport={}&format={}&force={}"
-    input_str = event.pattern_match.group(1)
-    response_api = requests.get(sample_url.format(
-        Config.SCREEN_SHOT_LAYER_ACCESS_KEY,
-        input_str,
-        "1",
-        "2560x1440",
-        "PNG",
-        "1"
-    ))
-    # https://stackoverflow.com/a/23718458/4723940
-    contentType = response_api.headers['content-type']
-    if "image" in contentType:
-        with io.BytesIO(response_api.content) as screenshot_image:
-            screenshot_image.name = "screencapture.png"
-            try:
-                await borg.send_file(
-                    event.chat_id,
-                    screenshot_image,
-                    caption=input_str,
-                    force_document=True,
-                    reply_to=event.message.reply_to_msg_id
-                )
-                await event.delete()
-            except Exception as e:
-                await event.edit(str(e))
-    else:
-        await event.edit(response_api.text)
 
 
 
@@ -605,8 +568,6 @@ CMD_HELP.update({
 \n**Usage:** Create a Channel..\
 \n\n`!poll` <reply to a poll>\
 \n**Usage:** Get poll details..\
-\n\n`!goss` <link>\
-\n**Usage:** Takes a screenshot of a website and sends the screenshot.\
 \n\n**All commands support sudo , type !help sudo for more info**\
 "
 })
