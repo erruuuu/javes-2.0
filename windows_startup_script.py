860f5c61b0688c7918e8040d958aadb91e950de7
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2020-11-28 23:25:30 +0530

    Delete windows_startup_script.py

--- windows_startup_script.py
+++
@@ -1,6 +0,0 @@
-from telethon import TelegramClient, events
-API_KEY="Type that here"
-API_HASH="Type that here"
-#get it from my.telegram.org
-bot = TelegramClient('userbot',API_KEY,API_HASH)
-bot.start()
