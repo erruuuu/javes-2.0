1c40470a0375dd93c3b22dee4a4f3f1a297ce28a
Sh1vam <Sh1vam@users.noreply.github.com>
2020-12-01 12:03:32 +0530

    hmmm

--- userbot/helpers/functions.py
+++ userbot/helpers/functions.py
@@ -116,8 +116,10 @@ async def extract_time(cat, time_val):
             # how even...?
             return ""
         return bantime
-    await cat.edit(
-        f"Invalid time type specified. Expected m , h , d or w but got: {time_val[-1]}"
+    cat.edit(
+        "Invalid time type specified. Expected m , h , d or w but got: {}".format(
+            time_val[-1]
+        )
     )
     return ""
 
