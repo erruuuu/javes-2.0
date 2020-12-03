71f5a3b58a80a749b11a95e54b9b1f37196093ea
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2020-12-03 22:16:17 +0530

    Update randoms.py

--- userbot/modules/randoms.py
+++ userbot/modules/randoms.py
@@ -400,3 +400,51 @@ async def lolmetrg(event):
     for files in (lolbruh, img):
         if files and os.path.exists(files):
             os.remove(files)'''
+@bot.on(admin_cmd(pattern=r"blur"))
+
+async def lolmetrg(event):
+    await event.delete()
+    sed = await event.get_reply_message()
+    if isinstance(sed.media, MessageMediaPhoto):
+        img = await borg.download_media(sed.media, sedpath)
+    elif "image" in sed.media.document.mime_type.split("/"):
+        img = await borg.download_media(sed.media, sedpath)
+    else:
+        await event.edit("Reply To Image")
+        return
+    url_s = upload_file(img)
+    imglink = f"https://telegra.ph{url_s[0]}"
+    lolul = f"https://some-random-api.ml/canvas/blur?avatar={imglink}"
+    r = requests.get(lolul)
+    open("shivam.png", "wb").write(r.content)
+    lolbruh = "shivam.png"
+    await borg.send_file(
+        event.chat_id, lolbruh, caption="blur..🤨", reply_to=sed
+    )
+    for files in (lolbruh, img):
+        if files and os.path.exists(files):
+            os.remove(files)
+@bot.on(admin_cmd(pattern=r"brpl"))
+
+async def lolmetrg(event):
+    await event.delete()
+    sed = await event.get_reply_message()
+    if isinstance(sed.media, MessageMediaPhoto):
+        img = await borg.download_media(sed.media, sedpath)
+    elif "image" in sed.media.document.mime_type.split("/"):
+        img = await borg.download_media(sed.media, sedpath)
+    else:
+        await event.edit("Reply To Image")
+        return
+    url_s = upload_file(img)
+    imglink = f"https://telegra.ph{url_s[0]}"
+    lolul = f"https://some-random-api.ml/canvas/blurple?avatar={imglink}"
+    r = requests.get(lolul)
+    open("shivam.png", "wb").write(r.content)
+    lolbruh = "shivam.png"
+    await borg.send_file(
+        event.chat_id, lolbruh, caption="blurple...😅", reply_to=sed
+    )
+    for files in (lolbruh, img):
+        if files and os.path.exists(files):
+            os.remove(files)
